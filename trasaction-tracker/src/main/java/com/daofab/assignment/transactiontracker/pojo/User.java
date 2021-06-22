bb7da40cc410ecc42456d7b465114c4560781045
i0b00j8 <bhaswant.inguva@walmartlabs.com>
2021-06-22 15:04:09 +0530

    initial skeleton

---
+++ trasaction-tracker/src/main/java/com/daofab/assignment/transactiontracker/pojo/User.java
@@ -0,0 +1,10 @@
+package com.daofab.assignment.transactiontracker.pojo;
+
+/**
+ * 
+ * @author i0b00j8
+ *
+ */
+public class User {
+
+}
---
+++ trasaction-tracker/src/main/java/com/daofab/assignment/transactiontracker/service/TransactionTrackerService.java
@@ -0,0 +1,13 @@
+package com.daofab.assignment.transactiontracker.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * @author i0b00j8
+ *
+ */
+@Service
+public class TransactionTrackerService {
+
+}
