eda70051667faa1a68a8238a07ebf0509e211cc9
i0b00j8 <bhaswant.inguva@walmartlabs.com>
2021-06-22 14:55:46 +0530

    Initial commit

---
+++ trasaction-tracker/src/main/java/com/daofab/assignment/trasactiontracker/TrasactionTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.daofab.assignment.trasactiontracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasactionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasactionTrackerApplication.class, args);
+	}
+
+}
