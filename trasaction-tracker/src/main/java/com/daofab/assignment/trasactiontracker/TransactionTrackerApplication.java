3a8bfca786ea25d7bca5f8624564de54535860d9
i0b00j8 <bhaswant.inguva@walmartlabs.com>
2021-06-24 22:16:33 +0530

    Returning without exceptio

--- trasaction-tracker/src/main/java/com/daofab/assignment/trasactiontracker/TransactionTrackerApplication.java
+++ trasaction-tracker/src/main/java/com/daofab/assignment/trasactiontracker/TransactionTrackerApplication.java
@@ -15,6 +15,7 @@ public class TransactionTrackerApplication {
 	public static void main(String[] args) {
 		if (args.length != 2) {
 			logger.error("Two arguments required. 1. Parent Json, 2. Child Json");
+			return;
 		}
 		DatabaseLoader.load(args[0], args[1]);
 		SpringApplication.run(TransactionTrackerApplication.class, args);
